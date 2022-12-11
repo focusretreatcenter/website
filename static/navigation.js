077e8f34ec280a1a373d23d6a96e141904bcd456
Peter Lyons <pete@focusretreatcenter.com>
2022-12-10 19:52:39 -0500

    Nav menu working

--- static/navigation.js
+++ static/navigation.js
@@ -1,23 +1,8 @@
 /* global document */
-const nav = document.querySelector("nav.full");
-// const navButton = document.querySelector("button.open-nav");
-// const content = document.querySelector(".content");
-
-function toggleNav() {
-  nav.classList.toggle("open");
-  // if (nav.classList.contains("open")) {
-  //   navButton.innerText = "×";
-  // } else {
-  //   navButton.innertText = "≡";
-  // }
-  // content.classList.toggle("navOpen");
-}
-
-function closeNav() {
-  nav.classList.remove("open");
-  // content.classList.remove("navOpen");
-}
-
-nav.addEventListener("click", toggleNav);
-document.querySelector("button.open-nav").addEventListener("click", toggleNav);
-document.querySelector("button.close-nav").addEventListener("click", toggleNav);
+const nav = document.querySelector("nav.menu");
+document
+  .querySelector("button.open-nav")
+  .addEventListener("click", () => nav.classList.toggle("open"));
+document
+  .querySelector("button.close-nav")
+  .addEventListener("click", () => nav.classList.remove("open"));
